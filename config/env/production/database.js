8bd27e5e7a082457b71f9e30738cd4d7ad3b3be3
Tomáš Tušer <tomastuser@seznam.cz>
2021-02-23 20:21:56 +0100

    render.yaml

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,22 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  };
+};
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("HEROKU_URL"),
+  host: "0.0.0.0",
+  url: env("RENDER_EXTERNAL_URL"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
 });
